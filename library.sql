c52611ef4e1144a9f5fba5b84b8764b9013fb85a
vitriolfox <vitriol.fox@gmail.com>
2017-10-30 17:08:12 +0100

    october 30

--- library.sql
+++ library.sql
@@ -26,7 +26,7 @@ CREATE TABLE `authors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,11 +49,15 @@ CREATE TABLE `books` (
   `title` varchar(100) DEFAULT NULL,
   `isbn` int(13) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `category` varchar(50) DEFAULT NULL,
-  `author` varchar(255) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`),
+  CONSTRAINT `books_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +66,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES ('Az utolsó egyszarvú',100,'Ez egy szép kakikakajaniku','Fantasy','Huppakakiku Marajanakiku',1),('Az elsö egyszarvú',100,'Ez lehetne szebb  kakikakajaniku','Fantasy','Huppakakiku Marajanakiku-sikuku',2),('Az egyszarvúak rendszertana',103,'Ez egy rendszerezett kaki','Fantasy','Huppakakiku Marajanakiku-sikuku',3),('Az egyszarvúak biológiai filantrópiája',104,'Ez egy filantrópikus kakiai összegzés','bio-Fantasy','Huppakakiku Marajanakiku-ramakakiku',4),('Az alklímiai egyszarvú',105,'Ez egy metafizikai kakika','Metantasy','Hermetakiku-Huppakakiku Marajanakiku',5),('A szarvatlan egyszarvú',106,'Ez egy szomoru kakika','Szomintasy','Szarvinuku-Huppakakiku Marajanakiku',6),('A nyomi egyszarvú',107,'Ez egy nyomott kakika','Nyomintasy','Kakiku Marajanakiku',7),('A 13 egyszarvú',108,'Ez egy számozott kakika','Nyomintasy','Kakiku Marajanakiku',8),('Az utolsóelötti egyszarvú',109,'Ez egy számozott kakikaka','Kakintasy','Kakiku Marajanakiku-kakiku',9),('Az átlagos egyszarvú',110,'Ez egy átlagos kakikaka','Utsontasy','Kakiku Marajanakiku-kakiku-uccsojo',10),('A pseudo egyszarvú',111,'Ez egy szép pseudo kaki','Pseudo-Fantasy','Huppakakiku Marajanakiku',11);
+INSERT INTO `books` VALUES ('Az utolsó egyszarvú',100,'Ez egy szép kakikakajaniku',2,NULL,1),('Az elsö egyszarvú',100,'Ez lehetne szebb  kakikakajaniku',3,NULL,2),('Az egyszarvúak rendszertana',103,'Ez egy rendszerezett kaki',NULL,NULL,3),('Az egyszarvúak biológiai filantrópiája',104,'Ez egy filantrópikus kakiai összegzés',NULL,NULL,4),('Az alklímiai egyszarvú',105,'Ez egy metafizikai kakika',3,NULL,5),('A szarvatlan egyszarvú',106,'Ez egy szomoru kakika',2,NULL,6),('A nyomi egyszarvú',107,'Ez egy nyomott kakika',NULL,NULL,7),('A 13 egyszarvú',108,'Ez egy számozott kakika',2,NULL,8),('Az utolsóelötti egyszarvú',109,'Ez egy számozott kakikaka',3,NULL,9),('Az átlagos egyszarvú',110,'Ez egy átlagos kakikaka',NULL,NULL,10),('A pseudo egyszarvú',111,'Ez egy szép pseudo kaki',NULL,NULL,11);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,7 +84,7 @@ CREATE TABLE `borrowed_books` (
   `borrowed_at` date DEFAULT NULL,
   `expire_at` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,9 +106,9 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
-  `description` text,
+  `description` mediumtext,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +135,7 @@ CREATE TABLE `enployees` (
   `position` varchar(50) DEFAULT NULL,
   `active` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,7 +161,7 @@ CREATE TABLE `visitors` (
   `joined_at` date DEFAULT NULL,
   `expire_at` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -178,4 +182,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-27 17:05:15
+-- Dump completed on 2017-10-30 17:07:36
